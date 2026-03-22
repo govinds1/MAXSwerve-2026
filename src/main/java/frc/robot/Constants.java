470be3423c3ea8b522c42fea440de49af8bc9647
Govind Suresh <govinds@umich.edu>
2026-03-22 01:59:47 -0400

    Increase max speed back to normal.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -32,8 +32,8 @@ public final class Constants {
   public static final class DriveConstants {
     // Driving Parameters - Note that these are not the maximum capable speeds of
     // the robot, rather the allowed maximum speeds
-    public static final double kMaxSpeedMetersPerSecond = 3;
-    public static final double kMaxAccelerationMetersPerSecondSquared = 2; // 11.8
+    public static final double kMaxSpeedMetersPerSecond = 4.8;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 3; // 11.8
     public static final double kMaxAngularSpeedRadiansPerSecond = 2 * Math.PI; // radians per second
     public static final double kMaxAngularAccelerationRadiansPerSecondSquared = Units.degreesToRadians(2430);
 
